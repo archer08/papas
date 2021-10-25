099ddffff6125abf778dee8e543012eeb5435119
dylan <dylanc@lavishflo.com>
2021-10-25 18:47:02 -0400

    twilio route change

--- config/twilloConfig.js
+++ config/twilloConfig.js
@@ -44,7 +44,7 @@ exports.twilloPtpConnectionRequestController = (req, res, next) => {
   const reciever = req.query.To;
   saveMessage(msg, sender, reciever);
   // const sortedDates = arrayOfDates.sort((dateA, dateB) => dateA.date - dateB.date)
-  const check = checkMessage({ msg });
+  const check = checkMessage(msg);
 
   twiml.message("hello");
 
