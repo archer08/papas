61989c03e5fc0a129385684ce9c9ddf2ede1aa7e
dylan <dylanc@lavishflo.com>
2021-12-01 20:53:06 -0500

    test

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ connectDb();
 const gql = require("./config/gql.js");
 gql();
 const express = require("express");
-const colors = require("colors");
-const twilloRoutes = require("./routes/twilloRoutes.js");
 const app = express();
-const userRoute = require("./routes/userRoutes.js");
 const cookieParser = require("cookie-parser");
 const sessions = require("express-session");
+const colors = require("colors");
+const twilloRoutes = require("./routes/twilloRoutes.js");
+const userRoute = require("./routes/userRoutes.js");
+
 // creating 24 hours from milliseconds
 const oneDay = 1000 * 60 * 60 * 24;
+app.use(cookieParser());
 
 //session middleware
 app.use(
@@ -23,13 +25,7 @@ app.use(
     resave: false,
   })
 );
-app.use(cookieParser());
 
-const profile = {
-  income: 0,
-  expences: 0,
-  transactions: {},
-};
 app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/twilio", twilloRoutes);
