4fd885313363bfe3b934beff8a29a764e5a0e044
dylan <dylanc@lavishflo.com>
2021-12-01 22:18:47 -0500

    test

--- index.js
+++ index.js
@@ -11,6 +11,29 @@ const sessions = require("express-session");
 const colors = require("colors");
 const twilloRoutes = require("./routes/twilloRoutes.js");
 const userRoute = require("./routes/userRoutes.js");
+const Vonage = require("@vonage/server-sdk");
+
+const vonage = new Vonage({
+  apiKey: "5a170902",
+  apiSecret: "Lfj8ib3oGIVR7Uts",
+});
+const from = "18773315585";
+const to = "19145136325";
+const text = "A text message sent using the Vonage SMS API";
+
+vonage.message.sendSms(from, to, text, (err, responseData) => {
+  if (err) {
+    console.log(err);
+  } else {
+    if (responseData.messages[0]["status"] === "0") {
+      console.log("Message sent successfully.");
+    } else {
+      console.log(
+        `Message failed with error: ${responseData.messages[0]["error-text"]}`
+      );
+    }
+  }
+});
 
 // creating 24 hours from milliseconds
 // const oneDay = 1000 * 60 * 60 * 24;
